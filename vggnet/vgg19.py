e8dc34da0fbaf9848d528fb6d0a6d00f8e8ae9f8
Patrick <dawnerz@163.com>
2019-12-06 20:34:43 +0800

    consider training

--- vggnet/vgg19.py
+++ vggnet/vgg19.py
@@ -1,17 +1,32 @@
 import torch.nn as nn
-import torch.utils.data.dataset as Dataset
+from torch.utils.data.dataset import Dataset
+from torch.utils.data.dataloader import DataLoader
+import torch.optim as optim
+
 
 class VGG19(nn.Module):
 
-    def __init__(self):
-        pass
+    # pool is "max" or "average"
+    def __init__(self, classNum, pool="max"):
+        self.conv1 = conv1(pool)
+        self.conv2 = conv2(pool)
+        self.conv3 = conv3(pool)
+        self.conv4 = conv4(pool)
+        self.conv5 = conv5(pool)
+        self.fullCon = fullConnectedSoftm(classNum)
 
     def forward(self, x):
-        pass
+        out1 = self.conv1(x)
+        out2 = self.conv2(out1)
+        out3 = self.conv3(out2)
+        out4 = self.conv4(out3)
+        out5 = self.conv5(out4)
+        return self.fullCon(out5)
+
 
 class TrainData(Dataset):
 
-    def __init__(self, file):
+    def __init__(self, fileDir):
         pass
 
     def __len__(self):
@@ -21,25 +36,33 @@ class TrainData(Dataset):
         pass
 
 
-def conv1Max():
+def poolSelect(pool):
+    if pool is "average":
+        return nn.AvgPool2d(kernel_size=2, stride=2)
+    return nn.MaxPool2d(kernel_size=2, stride=2)
+
+
+def conv1(pool="max"):
     return nn.Sequential(
         nn.Conv2d(3, 64, kernel_size=3, padding=1),
         nn.ReLU(True),
         nn.Conv2d(64, 64, kernel_size=3, padding=1),
         nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+        poolSelect(pool)
     )
 
-def conv2Max():
+
+def conv2(pool="max"):
     return nn.Sequential(
         nn.Conv2d(64, 128, kernel_size=3, padding=1),
         nn.ReLU(True),
         nn.Conv2d(128, 128, kernel_size=3, padding=1),
         nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+        poolSelect(pool)
     )
 
-def conv3Max():
+
+def conv3(pool="max"):
     return nn.Sequential(
         nn.Conv2d(128, 256, kernel_size=3, padding=1),
         nn.ReLU(True),
@@ -49,10 +72,11 @@ def conv3Max():
         nn.ReLU(True),
         nn.Conv2d(256, 256, kernel_size=3, padding=1),
         nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+        poolSelect(pool)
     )
 
-def conv4Max():
+
+def conv4(pool="max"):
     return nn.Sequential(
         nn.Conv2d(256, 512, kernel_size=3, padding=1),
         nn.ReLU(True),
@@ -62,10 +86,11 @@ def conv4Max():
         nn.ReLU(True),
         nn.Conv2d(512, 512, kernel_size=3, padding=1),
         nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+        poolSelect(pool)
     )
 
-def conv5Max():
+
+def conv5(pool="max"):
     return nn.Sequential(
         nn.Conv2d(512, 512, kernel_size=3, padding=1),
         nn.ReLU(True),
@@ -75,12 +100,13 @@ def conv5Max():
         nn.ReLU(True),
         nn.Conv2d(512, 512, kernel_size=3, padding=1),
         nn.ReLU(True),
-        nn.MaxPool2d(kernel_size=2, stride=2)
+        poolSelect(pool)
     )
+
 
 def fullConnectedSoftm(classNum):
     return nn.Sequential(
-        nn.Linear(512*7*7, 4096),
+        nn.Linear(512 * 7 * 7, 4096),
         nn.ReLU(True),
         nn.Dropout(),
         nn.Linear(4096, 4096),
@@ -89,3 +115,21 @@ def fullConnectedSoftm(classNum):
         nn.Linear(4096, classNum),
         nn.Softmax()
     )
+
+def loadModel():
+    pass
+
+def trainModel(vgg, dataLoader,loss,optim):
+    pass
+
+def testModel(vgg,testDataset):
+    pass
+
+if __name__=="__main__":
+    trainpath=""
+    testpath=""
+    vgg=VGG19()
+    trainset=TrainData(trainpath)
+    dataLoader=DataLoader(trainset,batch_size=256)
+    loss=nn.CrossEntropyLoss()
+    optim=optim.SGD()
