539260f8d9bab5bd9b004cc66aa98543315cb148
Patrick <dawnerz@163.com>
2019-12-19 10:08:10 +0800

    dataset augmentation

--- data/local_dataset.py
+++ data/local_dataset.py
@@ -10,7 +10,6 @@ import skimage.io as imgio
 from skimage.transform import resize
 import random
 
-
 if re.match(r'.*inux.*', sys.platform()):
     imagenetdir = r'/run/media/patrick/6114f130-f537-4999-b5f6-33fe2afc51db/imagenet12'
     cifar10dir = r'/run/media/patrick/6114f130-f537-4999-b5f6-33fe2afc51db/cifar10'
@@ -72,23 +71,23 @@ class Cifar10Test(Dataset):
 class ImagenetTrain(Dataset):
     def __init__(self):
         from scipy.io import loadmat
-        self.ID_lable_dict={}
-        self.ID_amount_dict={}
-        devkitdir=os.path.join(imagenetdir,imagenetSubpath['devkit']['t2'])
-        devkitdir=os.path.join(devkitdir,'data')
-        devkitmeta=os.path.join(devkitdir,'meta.mat')
-        mat=loadmat(devkitmeta)
+        self.ID_lable_dict = {}
+        self.ID_amount_dict = {}
+        devkitdir = os.path.join(imagenetdir, imagenetSubpath['devkit']['t2'])
+        devkitdir = os.path.join(devkitdir, 'data')
+        devkitmeta = os.path.join(devkitdir, 'meta.mat')
+        mat = loadmat(devkitmeta)
         for info in mat['synsets']:
-            id=info[0][0][1][0]
-            lable=info[0][0][0][0][0]
-            self.ID_lable_dict[id]=lable
+            id = info[0][0][1][0]
+            lable = info[0][0][0][0][0]
+            self.ID_lable_dict[id] = lable
         traindir = os.path.join(imagenetdir, imagenetSubpath['img']['train'])
-        self.dataLength=0;
+        self.dataLength = 0;
         for id in os.listdir(traindir):
-            dir=os.path.join(traindir,id)
-            length=len(os.listdir(dir))
-            self.ID_amount_dict[id]=length
-            self.dataLength+=length
+            dir = os.path.join(traindir, id)
+            length = len(os.listdir(dir))
+            self.ID_amount_dict[id] = length
+            self.dataLength += length
 
     def __len__(self):
         return self.dataLength
@@ -96,30 +95,60 @@ class ImagenetTrain(Dataset):
     def __getitem__(self, item):
 
         pass
-def dataAugment(originalImgArray,scale=range(256,513)):
+
+
+def dataAugment(originalImgArray, scale=range(256, 513)):
+    # cast to float
+    originalImgArray = originalImgArray.astype(np.float32)
+    # preprocesse to make it centered
+    mean = np.sum(originalImgArray) / originalImgArray.size
+    originalImgArray -= mean
     # multi-scale and reshape
-    shortLen=scale[random.randint(0,len(scale)-1)]
-    shape=originalImgArray.shape
-    if shape[0]>shape[1]:
-        shape[0]*=shortLen/shape[1]
-        shape[1]=shortLen
+    shortLen = scale[random.randint(0, len(scale) - 1)]
+    shape = originalImgArray.shape
+    if shape[0] > shape[1]:
+        shape[0] *= shortLen / shape[1]
+        shape[1] = shortLen
     else:
-        shape[1]*=shortLen/shape[0]
-        shape[0]=shortLen
-    scaledImg=resize(originalImgArray,shape).transpose((2,0,1))
+        shape[1] *= shortLen / shape[0]
+        shape[0] = shortLen
+    scaledImg = resize(originalImgArray, shape).transpose((2, 0, 1))
     # randome crop
-    shape=scaledImg.shape
-    h_limt=shape[0]-224
-    w_limt=shape[1]-224
-    h_offset=random.randint(0,h_limt)
-    w_offset=random.randint(0,w_limt)
-    cropped=scaledImg[:,h_offset:h_offset+225,w_offset:w_offset+225]
+    shape = scaledImg.shape
+    h_limt = shape[0] - 224
+    w_limt = shape[1] - 224
+    h_offset = random.randint(0, h_limt)
+    w_offset = random.randint(0, w_limt)
+    cropped = scaledImg[:, h_offset:h_offset + 224, w_offset:w_offset + 224]
     # horizontal reflection/flip
-    rand=random.randint(0,1)
+    rand = random.randint(0, 1)
     if rand > 0:
-        cropped=np.flip(cropped,2)
+        cropped = np.flip(cropped, 2)
     # alter RGB intensities
-        
+    alphas = np.random.normal(0, 0.1, 3)
+    for i in range(224):
+        for j in range(224):
+            origin = cropped[:, i, j].squeeze()
+            mean = origin / 3
+            row = origin.reshape((1, 3)) - mean
+            column = origin.reshape((3, 1)) - mean
+            cov = np.matmul(column, row)  # covariance
+            eigvals, eigvecs = np.linalg.eig(cov)  # eigen-decompose
+            sortedvals = np.sort(eigvals)
+            mat = eigvecs[eigvals.index(sortedvals[-1]), :]
+            mat.reshape((1, 3))
+            for k in range(2, 0, -1):
+                mat = np.concatenate([mat, eigvecs[eigvals.index(sortedvals[k])].reshape(1, 3)])
+            mat = mat.transpose((1, 0))
+            vec = np.array([alphas[0] * sortedvals[2], alphas[1] * sortedvals[1], alphas[2] * sortedvals[0]]).reshape(
+                (3, 1))
+            product = np.matmul(mat, vec).squeeze()
+            for k in range(0, 3):
+                cropped[k][i][j] += product[k]
+    # return ndarray
+    return cropped
+
+
 class ImagenetTest(Dataset):
     def __init__(self):
         pass
---
+++ nptest.py
@@ -0,0 +1,4 @@
+import numpy as np
+array=np.array([7,3,8,4,6,3,6,8])
+print(np.sort(array))
+print(array)
