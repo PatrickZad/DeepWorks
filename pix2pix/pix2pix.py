5fe9e055f5e582183baf30b75ff700e84253df8b
Patrick <dawnerz@163.com>
2019-12-26 22:57:26 +0800

    upsample padding

--- pix2pix/pix2pix.py
+++ pix2pix/pix2pix.py
@@ -45,17 +45,17 @@ class BasicEncoder(nn.Module):
 class BasicDecoder(nn.Module):
     def __init__(self, outchannels=3):
         super.__init__(BasicDecoder, self)
-        self.network = nn.Sequential(nn.Conv2d(512, 512, kernel_size=4, stride=2),
+        self.network = nn.Sequential(nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=4),
                                      nn.BatchNorm2d(),
                                      nn.Dropout2d(0.5),
-                                     nn.Conv2d(512, 512, kernel_size=4, stride=2),
+                                     nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=7),
                                      nn.Dropout2d(0.5),
-                                     nn.Conv2d(512, 512, kernel_size=4, stride=2),
+                                     nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=13),
                                      nn.Dropout2d(0.5),
-                                     nn.Conv2d(512,512,kernel_size=4,stride=2),
-                                     nn.Conv2d(512,256,kernel_size=4,stride=2),
-                                     nn.Conv2d(256,128,kernel_size=4,stride=2),
-                                     nn.Conv2d(128,64,kernel_size=4,stride=2),)
+                                     nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=25),
+                                     nn.Conv2d(512, 256, kernel_size=4, stride=2, padding=49),
+                                     nn.Conv2d(256, 128, kernel_size=4, stride=2, padding=97),
+                                     nn.Conv2d(128, 64, kernel_size=4, stride=2, padding=193), )
         pass
 
     def forward(self, input):
